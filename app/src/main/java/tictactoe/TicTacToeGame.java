0862df5b998d9622b4e73c93d554ee6d4c33383f
Dustin <46390912+dustin-mcdill@users.noreply.github.com>
2025-04-06 15:20:06 -0500

    Updated code for 2nd Portfolio

--- app/src/main/java/tictactoe/TicTacToeGame.java
+++ app/src/main/java/tictactoe/TicTacToeGame.java
@@ -5,18 +5,22 @@ public class TicTacToeGame {
     private GameBoard board;
     private char currentPlayer;
     private Scanner scanner;
+    private Log log; //Added the log to the game
 
     public TicTacToeGame(Scanner scanner) {
         board = new GameBoard();
         currentPlayer = 'X';
         this.scanner = scanner;
+        log = new Log(); //Initialized the log
     }
     
 
     public void play() {
         boolean playAgain;
+        char lastLoss = 'O'; //Tracker for loser
         do {
             board.resetBoard();
+            currentPlayer = (lastLoss == 'X') ? 'X':'O'; //Sets the first player based on the lastLoss
             System.out.println("Welcome to Tic-Tac-Toe!");
             boolean gameWon = false;
             int moves = 0;
@@ -31,6 +35,7 @@ public class TicTacToeGame {
                 if (board.checkWin(currentPlayer)) {
                     board.printBoard();
                     System.out.println("Player " + currentPlayer + " wins!");
+                    log.recordAwin(currentPlayer); //Logs the winner
                     gameWon = true;
                 } else {
                     currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
@@ -40,11 +45,12 @@ public class TicTacToeGame {
             if (!gameWon) {
                 board.printBoard();
                 System.out.println("It's a draw!");
+                log.recordAtie(); //Logs a tie
             }
-
+            log.printStat(); //Displays stats
             playAgain = askToPlayAgain();
         } while (playAgain);
-
+        log.saveFile(); //Saves log
         System.out.println("Goodbye!");
         scanner.close();
     }
